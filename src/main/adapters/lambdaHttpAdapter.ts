343b3f3530f3713f1b4de9779fd2071e426d0e3b
Kauê Leal de Lima <kaueslim@outlook.com>
2025-08-31 21:01:21 -0300

    Update lambdaHttpAdapter.ts

--- src/main/adapters/lambdaHttpAdapter.ts
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -1,8 +1,10 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { ZodError } from 'zod';
+
 import { Controller } from '@application/contracts/Controller';
 import { ErrorCode } from '@application/errors/ErrorCode';
 import { HttpError } from '@application/errors/http/HttpError';
+
 import { lambdaBodyParser } from '../utils/lambdaBodyParser';
 import { lambdaErrorResponse } from '../utils/lambdaErrorResponse';
 
@@ -22,6 +24,7 @@ export function lambdaHttpAdapter(controller: Controller) {
       return {
         statusCode: response.statusCode,
         body: response?.body ? JSON.stringify(response.body) : undefined,
+        isBase64Encoded: false,
       };
     } catch (error) {
       if (error instanceof ZodError) {
